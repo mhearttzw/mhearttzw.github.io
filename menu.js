a2d49aecc31755cafa948c6fb007b3a8b848fb9b
mhearttzw <mhearttzw@gmail.com>
2016-11-16 16:43:15 +0800

    11.16 16:43

--- menu.js
+++ menu.js
@@ -2,7 +2,11 @@ var menu = [
 	['第一阶段',
 		['ife/step_1/task_01/a.html', '任务一'],
 		['ife/step_1/task_02/a.html', '任务二'],
-		['ife/step_1/task_03/a.html', '任务三']
+		['ife/step_1/task_03/a.html', '任务三'],
+		['ife/step_1/task_04/a.html', '任务四'],
+		['ife/step_1/task_05/a.html', '任务五'],
+		['ife/step_1/task_06/a.html', '任务六'],
+		['ife/step_1/task_07/a.html', '任务七']
 	]
 ];
 
